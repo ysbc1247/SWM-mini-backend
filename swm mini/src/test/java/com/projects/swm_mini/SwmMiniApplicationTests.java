d352a45297cdb790e4acee057f1d00622ba2dcfe
ysbc1247 <ystc1247@khu.ac.kr>
2023-04-27 04:02:46 +0900

    [FEAT] initialized project

---
+++ swm mini/src/test/java/com/projects/swm_mini/SwmMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.swm_mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwmMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
