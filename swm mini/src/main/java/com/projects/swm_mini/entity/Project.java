48b4b08af996e6e2a5d305864475a00517406fe0
ysbc1247 <ystc1247@khu.ac.kr>
2023-04-27 04:04:38 +0900

    [FEAT] created project entity

--- swm mini/src/main/java/com/projects/swm_mini/entity/Project.java
+++ swm mini/src/main/java/com/projects/swm_mini/entity/Project.java
@@ -1,36 +1,51 @@
-package com.projects.swm_mini.enitty;
+package com.projects.swm_mini.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "article")
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "createdAt")
+})
 @Getter
 @Setter
 
-public class Project {
+public class Project extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private Long title;
+    private String projectName;
 
     @Column(nullable = false)
-    private Long username;
+    private String username;
 
     @Column(nullable = false)
-    private Long teamname;
+    private String teamName;
 
     @Column(nullable = false)
-    private Long date;
+    private String title;
 
-    @Column(nullable = false)
-    private Long title;
-    
-    @Column(nullable = false)
-    private Long title;
+    @Column(nullable = false, length = 1500)
+    private String description;
 
+    @Column
+    private String link;
+
+    protected  Project(){}
+    private Project(String projectName, String username, String teamName, String title, String description, String link){
+        this.projectName = projectName;
+        this.username = username;
+        this.teamName = teamName;
+        this.title = title;
+        this.description = description;
+        this.link = link;
+    }
+    public static Project of(String projectName, String username, String teamName, String title, String description, String link){
+        return new Project(projectName, username, teamName, title, description, link);
+    }
 
 }
